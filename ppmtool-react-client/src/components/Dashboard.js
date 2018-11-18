f8b96f63c4e9d2b239fd44a802d3da85334bc6ae
Zandraa <zandraa.perjesi@gmail.com>
2018-11-18 21:42:32 +0100

    Add function to get all projects from backend via api call

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react'
-import ProjectItem from './Project/ProjectItem';
-import CreateProjectButton from './Project/CreateProjectButton';
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+    //lsh
+    componentDidMount() {
+        this.props.getProjects();
+    }
+
     render() {
         return (
             <div className="projects">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center">Projects</h1>
-                    <br />
-                        <CreateProjectButton />
-                    <br />
-                    <hr />
-            
-                    <ProjectItem />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <CreateProjectButton />
+                            <br />
+                            <hr />
 
+                            <ProjectItem />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
         );
     }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    project: state.project
+});
+
+export default connect(
+    mapStateToProps,
+    { getProjects }
+)(Dashboard);
