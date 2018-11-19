0971c9e720b9334b3a29b90018a386c4dc683ad7
Zandraa <zandraa.perjesi@gmail.com>
2018-11-19 23:27:59 +0100

    Visualize data from Spring API

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -12,6 +12,8 @@ class Dashboard extends Component {
     }
 
     render() {
+        const { projects } = this.props.project;
+
         return (
             <div className="projects">
                 <div className="container">
@@ -23,7 +25,12 @@ class Dashboard extends Component {
                             <br />
                             <hr />
 
-                            <ProjectItem />
+                            {projects.map(project => (
+                                <ProjectItem
+                                    key={project.id}
+                                    project={project}
+                                />
+                            ))}
                         </div>
                     </div>
                 </div>
