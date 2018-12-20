22be03b709c21945ff8e0d90967905e37c4fc89d
Zandraa <zandraa.perjesi@gmail.com>
2018-12-20 22:06:19 +0100

    Make project task show error message when invalid values are sent to api

--- ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -24,6 +24,12 @@ class AddProjectTask extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+
     onChange(e) {
         this.setState({ [e.target.name]: e.target.value });
     }
@@ -48,6 +54,7 @@ class AddProjectTask extends Component {
 
     render() {
         const { id } = this.props.match.params;
+        const { errors } = this.state;
 
         return (
             <div className="add-PBI">
@@ -67,12 +74,22 @@ class AddProjectTask extends Component {
                                 <div className="form-group">
                                     <input
                                         type="text"
-                                        className="form-control form-control-lg"
+                                        className={classnames(
+                                            "form-control form-control-lg",
+                                            {
+                                                "is-invalid": errors.summary
+                                            }
+                                        )}
                                         name="summary"
                                         placeholder="Project Task summary"
                                         value={this.state.summary}
                                         onChange={this.onChange}
                                     />
+                                    {errors.summary && (
+                                        <div className="invalid-feedback">
+                                            {errors.summary}
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <textarea
@@ -139,10 +156,15 @@ class AddProjectTask extends Component {
 }
 
 AddProjectTask.PropTypes = {
-    addProjectTask: PropTypes.func.isRequired
+    addProjectTask: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     { addProjectTask }
 )(AddProjectTask);
