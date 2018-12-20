c9981253edf7e8b6e790edead40fbceb498b6c55
Zandraa <zandraa.perjesi@gmail.com>
2018-12-20 20:30:09 +0100

    Add projectboard

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,8 @@ import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
     render() {
@@ -26,6 +28,16 @@ class App extends Component {
                             exact
                             path="/updateProject/:id"
                             component={UpdateProject}
+                        />
+                        <Route
+                            exact
+                            path="/projectBoard/:id"
+                            component={ProjectBoard}
+                        />
+                        <Route
+                            exact
+                            path="/addProjectTask/:id"
+                            component={AddProjectTask}
                         />
                     </div>
                 </Router>
